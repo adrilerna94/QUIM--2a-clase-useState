b66717b19052209ba9323edc2b50187bc4e52aa4
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-03-27 10:15:54 +0100

    setup

---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+import { Button, Card, CardBody, Typography } from "@/components/ui";
+import { WelcomeMessage } from "./welcome/page";
+import { FormPage } from "./contact/page";
+
+export default function Home() {
+  return (
+    <main className="flex items-center min-h-screen justify-center bg-gray-900 text-white">
+      <Card className="w-full max-w-lg shadow-xl border border-gray-700 bg-gray-800">
+        <CardBody className="text-center flex flex-col gap-4">
+          <Typography variant="h1" color="amber" className="font-bold">
+            🚀 Bienvenidos a la Misión: Red Planet
+          </Typography>
+        </CardBody>
+      </Card>
+    </main>
+  );
+}
